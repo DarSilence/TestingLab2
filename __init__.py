28460f064b4f149dc64ce9b9b8b149801087290b
DarSilence <144653580+DarSilence@users.noreply.github.com>
2025-11-30 21:11:12 +0300

    Uploaded file for package

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .for_bd import *
+from .Log_in_frame import *
+from .Register_frame import *
+from .Main_frame import *
+from .Bought_frame import *
+from .Return_frame import *
+from .Main_window import *
